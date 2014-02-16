bbb23db5a5c7bd32cf5d60ad2a00e6fde136df34
Sanjeev Murty <murty.sanjeev@gmail.com>
2014-02-15 20:15:13 -0700

    Created converter.py

---
+++ converter.py
@@ -0,0 +1,26 @@
+import re
+from parser import Parser
+
+class Converter(object):
+	"""Takes a string that represents a raw
+	Lithp program, and converts it to C++.
+	"""
+
+	def __init__(self, program):
+		self.program = program
+		self.parser = Parser(program)
+		self.tokens = self.parser.get_tokens()
+		self.convert() #sets self.converted
+
+	def convert(self):
+		"""
+		Converts the program into C++ code
+		Code must be compiled wth lithp.hpp
+		"""
+
+		self.converted = ''
+
+		return self.converted
+
+	def get_cpp(self):
+		return self.converted
