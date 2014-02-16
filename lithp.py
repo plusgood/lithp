9fead86bc1f35b40b5e29cc7e5074a645539b22e
Sanjeev Murty <murty.sanjeev@gmail.com>
2014-02-15 20:16:49 -0700

    Added main file lithp.py

---
+++ lithp.py
@@ -0,0 +1,30 @@
+from __future__ import print_function
+from converter import Converter
+import sys
+
+def main(argv):
+	fp = argv[1]
+	try:
+		with open(fp) as f:
+			program = f.read()
+	except IOError:
+		print("File not found", file=sys.stderr)
+		return 1
+
+	converter = Converter(program)
+
+	dot_index = fp.rfind('.')
+	if dot_index == -1:
+		name = fp
+	else:
+		name = fp[:dot_index]
+
+	with open(name+'.cpp', 'w') as f:
+		f.write(converter.get_cpp())
+	
+	#-O2 is especially necessary for tail recursion optimization
+	return os.system('g++ -O2 -o %s.exe %s.cpp' % name)
+
+
+if __name__ == '__main__':
+	exit(main(sys.argv))
