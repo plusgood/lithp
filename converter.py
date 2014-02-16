b07f0aecd95017bbb37c0d19d846974b6233a5cd
Sanjeev Murty <murty.sanjeev@gmail.com>
2014-02-16 14:42:45 -0700

    Added make_func_declarations and convert_type stub to Converter

--- converter.py
+++ converter.py
@@ -11,6 +11,7 @@ class Converter(object):
 		self.lexer = Lexer(program)
 		self.tokens = self.lexer.get_tokens()
 		self.convert() #sets self.converted
+		self.typedefs = {}
 
 	def convert(self):
 		"""Converts the program into C++ code
@@ -85,8 +86,68 @@ class Converter(object):
 			self.func_dict[name] = self.func_dict[name].replace('self', name)
 
 	def make_func_declarations(self):
-		pass
+		"""Creates function declarations with parameter
+		types and return types. Higher order functions
+		are supported. Function declarations are also
+		used as function signatures.
 
+		Precondition: make_func_dict must have been called
+		"""
+		self.declarations = {} #{func_name : func_decl, ...}
+		for name in self.func_dict:
+			body = Lexer(self.func_dict[name]).get_tokens()
+
+			i = body.index('\\') #Lambda
+
+			#Start of parameters
+			while body[i] != '(': i += 1
+
+			j = body.match_paren(i)
+			param_list = body[i + 1: j] #Stuff inside parentheses
+
+			index = i+1
+			params = []
+			acc = '' #token accumulator
+
+			while index < len(param_list):
+				if param_list[index] == ',':
+					#Reached end of a parameter
+					var_name, type_str = acc.split(':')
+					params.append(self.convert_type(var_name, type_str))
+					acc = ''
+				elif param_list[index] == '(':
+					#Skip over parameters that are functions
+					index = param_list.match_paren(index)
+				else:
+					acc += param_list[index]
+
+				index += 1
+
+			return_type = ''
+			if body[j+2] == '(': #Function returns another function
+				#                                  +3 for [")","->","<type>"]
+				for x in xrange(j+2, body.match_paren(index)+3):
+					return_type += body[x]
+			else: #Function returns a concrete type
+				return_type = body[j+2] #Plus two to skip over ")" and ":"
+
+			func_type = self.convert_type(name, return_type)
+			self.declarations[name] = func_type + '(' + ', '.join(params) + ')'
+			
+
+	def convert_type(self, name, type):
+		"""Converts a string that constitutes a type
+		that may be simple (e.g. x:int, x:double)
+		or a function (e.g. x:(int,int)->int) into its
+		C++ definition. `name` may be an empty string.
+		This function may create a typedef and add it 
+		to self.typedefs in which case it will return
+		the new name.
+		"""
+		
+		#Might need recursion due to higher order functions
+		pass
+			
 	def make_main_method(self):
 		pass
 
