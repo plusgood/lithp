c88c105f9fd6b20edb00908e9ecbffc106b9474c
Sanjeev Murty <murty.sanjeev@gmail.com>
2014-02-20 19:41:33 -0700

    Added default step of 1 to range

--- lithp.hpp
+++ lithp.hpp
@@ -69,7 +69,7 @@ T1 foldl(T1 (*func)(T1, T2), T1 start, vector<T2> v){
   return start;
 }
 
-vector<int> range(int start, int stop, int step){
+vector<int> range(int start, int stop, int step=1){
   vector<int> v;
   for(int i = start; i < stop; i += step){
     v.push_back(i);
